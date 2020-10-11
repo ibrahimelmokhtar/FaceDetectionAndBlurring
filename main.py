1b40a9a52a4d4010995381395ee11f8a780cb968
Ibrahim El-Mokhtar <ibrahimelmokhtar@gmail.com>
2020-10-11 13:13:03 +0200

    Detect faces from input video

--- main.py
+++ main.py
@@ -16,6 +16,12 @@ while(True):
     video_gray = cv2.cvtColor(video_input, cv2.COLOR_BGR2GRAY)
     cv2.imshow('Gray Video', video_gray)
 
+    # detect faces in input video (after gray scaling):
+    faces = FaceDetector.detectMultiScale(video_gray, 1.2, 5)
+
+    # Print out the number of found faces:
+    print(f"Faces found: {len(faces)} \t You can quit the program by pressing 'q' or 'Q'")
+
     # to QUIT the program: >>> press (q) OR (Q) button
     key = cv2.waitKey(1) & 0xFF
     if (key == ord('q')) | (key == ord('Q')):
